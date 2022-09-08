1e37b36fc88a24c22d382776a6540e36ece577ca
musta <mustafa774519161@gmail.com>
2022-09-08 22:00:41 +0300

    Read Users from Server Library

--- app/src/main/java/com/example/readusersfromserverlibrary/MainActivity.java
+++ app/src/main/java/com/example/readusersfromserverlibrary/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.read_users_from_server_library.ReadUsers;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
